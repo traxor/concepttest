e66f227911d96f67a921825a96a389f02acf677b
Daniel Östberg <danielostberg@hotmail.com>
2013-04-18 17:51:39 +0200

    Added option to reduce amount of answers
    The AddQuestion page now also work in IE and Firefox

--- Script.js
+++ Script.js
@@ -1,32 +1,36 @@
 var i = 0;
 var j = 10;
 
+/*This function could probably be improved*/
 function addform() {
+    
+    if (i > 9)
+        return;
 
     var parentdiv = document.getElementById("answerfield");
 
-    document.getElementById("counter").value++;
+    parseInt(document.getElementById("counter")).value++;
                         
     i++;
     j++;
 
-    var text = document.createElement();
+    var text = document.createElement("text");
     text.innerHTML = "Answer " + (i) + ": ";
 
-    var correcttext = document.createElement();
-    correcttext.innerHTML = "Correct: ";
+    var correcttext = document.createElement("text");
+    correcttext.innerHTML = " Correct: ";
 
-    var newrow = document.createElement();
+    var newrow = document.createElement("text");
     newrow.innerHTML = "<br/>";
 
     var Answer = document.createElement("input");
-    Answer.setAttribute("type", "text");
-    Answer.setAttribute("name", i);
-    Answer.setAttribute("value", "");
+    Answer.type = "text"
+    Answer.id = i;
+    Answer.value = "";
 
     var checkbox = document.createElement("input");
     checkbox.type = "Checkbox";
-    checkbox.name = j;
+    checkbox.id = j;
     checkbox.value = "correct";
 
     parentdiv.appendChild(text);
@@ -35,6 +39,26 @@ function addform() {
     parentdiv.appendChild(checkbox);
     parentdiv.appendChild(newrow);
 
+}
+
+function removeform(){
+
+    if (i <= 0)
+        return;
+
+    var parentdiv = document.getElementById("answerfield");
+
+    document.getElementById("counter").value--;
+
+    var answer = document.getElementById(i);
+    var checkbox = document.getElementById(j);
+
+    i--;
+    j--;
+
+    for (k = 0; k < 5; k++) {
+        parentdiv.removeChild(parentdiv.lastChild);
+    }
 }
 
 function updatelectures() {
